55df7ef5c206466fbcab83e7735005c707bc75a7
parallaxD <denis_kuplevatskiy@mail.ru>
2023-05-06 10:43:51 +0500

    Прибрался в архитектуре

---
+++ Managers/EnemyManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlearnGame
+{
+    public static class EnemyManager
+    {
+        public static List<Enemy> Enemies { get; } = new List<Enemy>();
+        private static Texture2D _texture;
+
+        public static void Initialize(string textureName)
+        {
+            _texture = Globals.Content.Load<Texture2D>(textureName);
+        }
+
+        public static void AddEnemy(Vector2 enemyPosition, int enemySpeed, int enemyHealth)
+        {
+            Enemies.Add(new Enemy(_texture, enemyPosition, enemySpeed, enemyHealth, 5));
+        }
+        public static void Update(Player player)
+        {
+            foreach (var enemy in Enemies)
+            {
+                enemy.Update(player);
+            }
+            Enemies.RemoveAll(enemy => enemy.Health <= 0);
+        }
+
+        public static void Draw()
+        {
+            foreach (var enemy in Enemies) enemy.Draw(Globals.SpriteBatch, 1, enemy.Rotation, SpriteEffects.None);
+        }
+    }
+}
--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -17,21 +17,26 @@ namespace UlearnGame
             _player = new Player(Globals.Content.Load<Texture2D>("wizzard_f_idle_anim_f0"),
                 new Vector2(Globals._windowWidth / 2, Globals._windowHeight / 2),
                 300);
-            ProjectileManager.Initialize();
 
+            ProjectileManager.Initialize("knight_f_idle_anim_f0");
+            EnemyManager.Initialize("big_demon_idle_anim_f0");
+            EnemyManager.AddEnemy(new Vector2(1000, 500), 50, 100);
             UIManager.Initialize();
         }
         public void Update()
         {
-            _player.Update();
-            ProjectileManager.Update();
+            _player.Update(EnemyManager.Enemies);
+            UIManager.Update();
+            ProjectileManager.Update(EnemyManager.Enemies);
             InputManager.Update();
+            EnemyManager.Update(_player);
         }
         public void Draw()
         {
             UIManager.Draw();
             ProjectileManager.Draw();
-            _player.Draw(Globals.SpriteBatch, 2, 0);
+            EnemyManager.Draw();
+            _player.Draw(Globals.SpriteBatch, 2);
         }
     }
 }
